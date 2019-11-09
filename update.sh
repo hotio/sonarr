9d2c01909833d03cf6d9841d3296e15f739e97bc
hotio <hotio@protonmail.com>
2019-11-09 15:58:22 +0100

    add manifest

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ location=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "${location}" || exit 1
 
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG ${app}_VERSION=.*$/ARG ${app}_VERSION=${version}/g" {} \;
+sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 
 if [[ -n "$(git status --untracked-files=no --porcelain)" ]]; then
     git add ./*.Dockerfile
