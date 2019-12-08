947e05b9b711032671d14a199fc2fc437f246dd9
hotio <hotio@protonmail.com>
2019-12-08 06:16:13 +0100

    screenshot

---
+++ puppeteer.js
@@ -0,0 +1,20 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    bindAddress: "0.0.0.0",
+    args: [
+      "--no-sandbox",
+      "--headless",
+      "--disable-gpu",
+      "--disable-dev-shm-usage",
+      "--remote-debugging-port=9222",
+      "--remote-debugging-address=0.0.0.0"
+    ]
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 2000 });
+  await page.goto("http://service:8989", { waitUntil: "networkidle2" });
+  await page.screenshot({ path: "/tmp/screenshot.png" });
+  await browser.close();
+})();
