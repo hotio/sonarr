31ddae043efc52c8e9e966827d7bbb5836caf311
hotio <hotio@protonmail.com>
2019-12-14 13:09:03 +0100

    move to script

--- update.sh
+++ update.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
-version=$(curl -fsSL "https://services.sonarr.tv/v1/download/phantom-develop?version=3" | jq -r .version)
-[[ -z ${version} ]] && exit
-find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG SONARR_VERSION=.*$/ARG SONARR_VERSION=${version}/g" {} \;
-sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
-echo "##[set-output name=version;]${version}"
+if [[ ${1} == "screenshot" ]]; then
+    SERVICE_IP="http://$(dig +short service):8989/system/status"
+    NETWORK_IDLE="2"
+    cd /usr/src/app && node <<EOF
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    bindAddress: "0.0.0.0",
+    args: [
+      "--no-sandbox",
+      "--headless",
+      "--disable-gpu",
+      "--disable-dev-shm-usage",
+      "--remote-debugging-port=9222",
+      "--remote-debugging-address=0.0.0.0"
+    ]
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.goto("${SERVICE_IP}", { waitUntil: "networkidle${NETWORK_IDLE}" });
+  await page.evaluate(() => {
+    const div = document.createElement('div');
+    div.innerHTML = 'Image: ${DRONE_REPO_OWNER}/${DRONE_REPO_NAME##docker-}:${DRONE_COMMIT_BRANCH}<br>Commit: ${DRONE_COMMIT_SHA:0:7}<br>Build: #${DRONE_BUILD_NUMBER}<br>Timestamp: $(date -u --iso-8601=seconds)';
+    div.style.cssText = "all: initial; font-weight: normal; font-size: normal; font-family: monospace; padding: 5px; color: white; position: fixed; bottom: 10px; right: 10px; background: DODGERBLUE; z-index: 10000";
+    document.body.appendChild(div);
+  });
+  await page.screenshot({ path: "/drone/src/screenshot.png", fullPage: true });
+  await browser.close();
+})();
+EOF
+    curl -fsSL -F'file=@/drone/src/screenshot.png' https://0x0.st > "/drone/src/screenshot.log"
+elif [[ ${1} == "checkservice" ]]; then
+    SERVICE="http://service:8989"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL ${SERVICE} > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    curl -fsSL ${SERVICE} > /dev/null
+else
+    version=$(curl -fsSL "https://services.sonarr.tv/v1/download/phantom-develop?version=3" | jq -r .version)
+    [[ -z ${version} ]] && exit
+    find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG SONARR_VERSION=.*$/ARG SONARR_VERSION=${version}/g" {} \;
+    sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
+    echo "##[set-output name=version;]${version}"
+fi
