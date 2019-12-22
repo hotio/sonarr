dfbb4c31bcbe8a2c82228f43619f0acec346ea69
hotio <hotio@protonmail.com>
2019-12-22 21:30:58 +0100

    add packages check

--- update.sh
+++ update.sh
@@ -35,6 +35,11 @@ elif [[ ${1} == "checkservice" ]]; then
     SERVICE="http://service:8989"
     currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL ${SERVICE} > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
     curl -fsSL ${SERVICE} > /dev/null
+elif [[ ${1} == "checkpackages" ]]; then
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/mono:stable-linux-arm64 bash -c 'apt list --installed > /github/upstream_packages.arm64.txt'
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/mono:stable-linux-arm   bash -c 'apt list --installed > /github/upstream_packages.arm.txt'
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/mono:stable-linux-amd64 bash -c 'apt list --installed > /github/upstream_packages.amd64.txt'
 else
     version=$(curl -fsSL "https://services.sonarr.tv/v1/download/phantom-develop?version=3" | jq -r .version)
     [[ -z ${version} ]] && exit
