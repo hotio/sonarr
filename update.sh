6fd60567af6718bffb4f20ad67de987e3100b1bb
hotio <hotio@protonmail.com>
2020-01-09 10:26:01 +0100

    use ping

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ${1} == "screenshot" ]]; then
-    SERVICE_IP="http://$(dig +short service):8989/system/status"
+    SERVICE_IP="http://$(ping -c1 -4 service | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'):8989/system/status"
     NETWORK_IDLE="2"
     cd /usr/src/app && node <<EOF
 const puppeteer = require('puppeteer');
