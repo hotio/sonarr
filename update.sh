66af9037a7ce3ccefd932913edfb901ad300c6ef
hotio <26902309+hotio@users.noreply.github.com>
2020-08-07 20:06:15 +0200

    update

--- update.sh
+++ update.sh
@@ -1,45 +1,8 @@
 #!/bin/bash
 
-if [[ ${1} == "screenshot" ]]; then
-    SERVICE_IP="http://${2}:8989/system/status"
-    NETWORK_IDLE="2"
-    node <<EOF
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch({
-    bindAddress: "0.0.0.0",
-    args: [
-      "--no-sandbox",
-      "--headless",
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-      "--remote-debugging-port=9222",
-      "--remote-debugging-address=0.0.0.0"
-    ]
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  try {
-    await page.goto("${SERVICE_IP}", { waitUntil: "networkidle${NETWORK_IDLE}" });
-  } catch (e) {
-    console.log(e)
-    process.exit(1)
-  }
-  await page.evaluate(() => {
-    const div = document.createElement('div');
-    div.innerHTML = 'Image: ${DRONE_REPO_OWNER}/${DRONE_REPO_NAME##docker-}:${DRONE_COMMIT_BRANCH}<br>App Version: ${VERSION_FIELD}<br>Commit: ${DRONE_COMMIT_SHA:0:7}<br>Build: #${DRONE_BUILD_NUMBER}<br>Timestamp: $(date -u --iso-8601=seconds)';
-    div.style.cssText = "all: initial !important; border-radius: 4px !important; font-weight: normal !important; font-size: normal !important; font-family: monospace !important; padding: 10px !important; color: black !important; position: fixed !important; bottom: 10px !important; right: 10px !important; background-color: #e7f3fe !important; border-left: 6px solid #2196F3 !important; z-index: 10000 !important";
-    document.body.appendChild(div);
-  });
-  await page.screenshot({ path: "screenshot.png", fullPage: true });
-  await browser.close();
-})();
-EOF
-elif [[ ${1} == "checkservice" ]]; then
-    SERVICE="http://${2}:8989"
-    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${SERVICE}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
-    curl -fsSL "${SERVICE}" > /dev/null
+if [[ ${1} == "checkservice" ]]; then
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${2}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    curl -fsSL "${2}" > /dev/null
 elif [[ ${1} == "checkdigests" ]]; then
     mkdir ~/.docker && echo '{"experimental": "enabled"}' > ~/.docker/config.json
     image="hotio/mono"
