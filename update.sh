c3adb7d930d6ce86723474eaacee41d08123e97a
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 20:28:28 +0200

    rework build

--- update.sh
+++ update.sh
@@ -9,9 +9,27 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apt list --installed
+    echo "Check if app works..."
+    app_url="http://localhost:8989/system/status"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    curl -fsSL "${app_url}" > /dev/null
+    status=$?
+    echo "Show docker logs..."
+    docker logs service
+    exit ${status}
+elif [[ ${1} == "screenshot" ]]; then
+    app_url="http://localhost:8989/system/status"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    docker run --rm --network host --entrypoint="" -u "$(id -u "$USER")" -v "${GITHUB_WORKSPACE}":/usr/src/app/src zenika/alpine-chrome:with-puppeteer node src/puppeteer.js
+    exit 0
 else
     version=$(curl -fsSL "https://services.sonarr.tv/v1/download/develop" | jq -r .version)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{SONARR_VERSION=[^}]*}/{SONARR_VERSION=${version}}/g" .github/workflows/build.yml
+    echo "VERSION=${version}" > VERSION
     echo "##[set-output name=version;]${version}"
 fi
