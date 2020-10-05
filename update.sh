99031bf53942e0c49a900c7604f941c9e2256de6
hotio <26902309+hotio@users.noreply.github.com>
2020-10-05 11:03:43 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -32,8 +32,6 @@ else
     [[ -z ${version} ]] && exit 1
     version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/docker-hotio/arr-discord-notifier/tags" | jq -r .[0].name)
     [[ -z ${version_arr_discord_notifier} ]] && exit 1
-    echo "VERSION=${version}" > VERSION
-    echo "ARR_DISCORD_NOTIFIER_VERSION=${version_arr_discord_notifier}" >> VERSION
-    echo '{"version":"'"${version}"'"}' > VERSION.json
+    echo '{"version":"'"${version}"'","arr_discord_notifier_version":"'"${version_arr_discord_notifier}"'"}' | jq . > VERSION.json
     echo "##[set-output name=version;]${version}/${version_arr_discord_notifier}"
 fi
