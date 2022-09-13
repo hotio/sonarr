62bc155967f52eacf8dfdb19b9379d3ff81488c7
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-13 09:52:58 +0200

    skip ci

---
+++ update-versions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+json=$(curl -fsSL "https://services.sonarr.tv/v1/update/develop/changes?version=4.0&os=linux" | jq '.[0]')
+version=$(jq -r '.version' <<< "${json}")
+[[ -z ${version} ]] && exit 0
+sbranch=$(jq -r '.branch' <<< "${json}")
+[[ -z ${sbranch} ]] && exit 0
+version_arr_discord_notifier=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hotio/arr-discord-notifier/tags" | jq -r .[0].name)
+[[ -z ${version_arr_discord_notifier} ]] && exit 0
+version_json=$(cat ./VERSION.json)
+jq '.version = "'"${version}"'" | .sbranch = "'"${sbranch}"'" | .arr_discord_notifier_version = "'"${version_arr_discord_notifier}"'"' <<< "${version_json}" > VERSION.json
