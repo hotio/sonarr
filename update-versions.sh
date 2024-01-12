5505524bcde7121c664a7a9145311b41fe88653a
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-01-12 07:58:27 +0100

    add changelog

--- update-versions.sh
+++ update-versions.sh
@@ -2,5 +2,8 @@
 json=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sonarr/sonarr/releases")
 version=$(jq -r .[0].tag_name <<< "${json}" | sed s/v//g)
 [[ -z ${version} ]] && exit 0
+old_version=$(jq -r '.version' < VERSION.json)
+changelog=$(jq -r '.changelog' < VERSION.json)
+[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/sonarr/sonarr/compare/v${old_version}...v${version}"
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
