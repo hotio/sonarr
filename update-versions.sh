05edbd6a4a687364608befed194b12129e2f9bda
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-01-12 07:20:51 +0100

    fix build

--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-json=$(curl -fsSL "https://services.sonarr.tv/v1/releases" | jq '.["v4-stable"]')
-version=$(jq -r '.version' <<< "${json}")
+json=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sonarr/sonarr/releases/latest")
+version=$(jq -r .tag_name <<< "${json}" | sed s/v//g)
 [[ -z ${version} ]] && exit 0
-sbranch=$(jq -r '.branch' <<< "${json}")
-[[ -z ${sbranch} ]] && exit 0
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .sbranch = "'"${sbranch}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'"' <<< "${version_json}" > VERSION.json
