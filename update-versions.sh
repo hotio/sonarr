518faf8e8a6db40427c1de639130eff29e0d183e
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 10:40:04 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 response_json=$(curl -fsSL "https://services.sonarr.tv/v1/releases" | jq -e '.["v4-nightly"]')
-version=$(jq -r .version <<< "${response_json}")
-sbranch=$(jq -r .branch <<< "${response_json}")
-amd64_url=$(jq -r '.linuxMusl.x64.archive.url' <<< "${response_json}")
-arm64_url=$(jq -r .linuxMusl.arm64.archive.url <<< "${response_json}")
+version=$(jq -re .version <<< "${response_json}")
+sbranch=$(jq -re .branch <<< "${response_json}")
+amd64_url=$(jq -re '.linuxMusl.x64.archive.url' <<< "${response_json}")
+arm64_url=$(jq -re .linuxMusl.arm64.archive.url <<< "${response_json}")
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
