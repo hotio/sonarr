a9ddfb2c6f2c48c370d14e9c53b5350532a8348c
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 09:49:47 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-json=$(curl -fsSL "https://services.sonarr.tv/v1/releases" | jq '.["v4-stable"]')
-version=$(jq -r .version <<< "${json}" | sed s/v//g)
-[[ -z ${version} ]] && exit 0
-sbranch=$(jq -r .branch <<< "${json}")
-[[ -z ${sbranch} ]] && exit 0
-amd64_url=$(jq -r '.linuxMusl.x64.archive.url' <<< "${json}")
-[[ -z ${amd64_url} ]] && exit 0
-arm64_url=$(jq -r .linuxMusl.arm64.archive.url <<< "${json}")
-[[ -z ${arm64_url} ]] && exit 0
-old_version=$(jq -r '.version' < VERSION.json)
-changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/sonarr/sonarr/compare/v${old_version}...v${version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .sbranch = "'"${sbranch}"'" | .amd64_url = "'"${amd64_url}"'" | .arm64_url = "'"${arm64_url}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
+set -e
+response_json=$(curl -fsSL "https://services.sonarr.tv/v1/releases" | jq -e '.["v4-stable"]')
+version=$(jq -re .version <<< "${response_json}")
+sbranch=$(jq -re .branch <<< "${response_json}")
+amd64_url=$(jq -re '.linuxMusl.x64.archive.url' <<< "${response_json}")
+arm64_url=$(jq -re .linuxMusl.arm64.archive.url <<< "${response_json}")
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg sbranch "${sbranch}" \
+    --arg amd64_url "${amd64_url}" \
+    --arg arm64_url "${arm64_url}" \
+    '.version = $version | .sbranch = $sbranch | .amd64_url = $amd64_url | .arm64_url = $arm64_url' <<< "${json}" | tee VERSION.json
