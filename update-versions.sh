171754fe5fb7eb067b1f381fc321375cc3a7c658
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 09:21:00 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-response_json=$(curl -fsSL "https://services.sonarr.tv/v1/releases" | jq -e '.["v4-nightly"]') || exit 1
+set -exuo pipefail
+
+response_json=$(curl -fsSL "https://services.sonarr.tv/v1/releases" | jq -e '.["v4-nightly"]')
 version=$(jq -re .version <<< "${response_json}")
 sbranch=$(jq -re .branch <<< "${response_json}")
 amd64_url=$(jq -re '.linuxMusl.x64.archive.url' <<< "${response_json}")
