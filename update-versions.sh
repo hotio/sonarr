8d8dfa0606cd1be7ee527846ba0bf9cd4878223d
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-13 09:50:00 +0100

    rename args [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -3,13 +3,13 @@ set -exuo pipefail
 
 response_json=$(curl -fsSL "https://services.sonarr.tv/v1/releases" | jq -e '.["v4-stable"]')
 version=$(jq -re .version <<< "${response_json}")
-sbranch=$(jq -re .branch <<< "${response_json}")
-amd64_url=$(jq -re '.linuxMusl.x64.archive.url' <<< "${response_json}")
-arm64_url=$(jq -re .linuxMusl.arm64.archive.url <<< "${response_json}")
+version_branch=$(jq -re .branch <<< "${response_json}")
+version_amd64_url=$(jq -re '.linuxMusl.x64.archive.url' <<< "${response_json}")
+version_arm64_url=$(jq -re .linuxMusl.arm64.archive.url <<< "${response_json}")
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg sbranch "${sbranch}" \
-    --arg amd64_url "${amd64_url}" \
-    --arg arm64_url "${arm64_url}" \
-    '.version = $version | .sbranch = $sbranch | .amd64_url = $amd64_url | .arm64_url = $arm64_url' <<< "${json}" | tee meta.json
+    --arg version_branch "${version_branch}" \
+    --arg version_amd64_url "${version_amd64_url}" \
+    --arg version_arm64_url "${version_arm64_url}" \
+    '.version = $version | .version_branch = $version_branch | .version_amd64_url = $version_amd64_url | .version_arm64_url = $version_arm64_url' <<< "${json}" | tee meta.json
